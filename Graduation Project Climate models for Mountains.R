6cc4861472d378e0573cb2c435de3b85c413eea0
RemkoPape <remkopape@gmail.com>
2024-03-18 16:18:27 +0100

    frs
    dsa
    
    fds
    
    dsa
    
    addition of readme file
    
    update
    
    remove
    
    Delete GMBA_2021_Ruggedness_EarthEnv_30s_V2.tif
    
    Update .gitignore
    
    Add design file
    
    Initial Setup and addition of data

---
+++ Graduation Project Climate models for Mountains.R
@@ -0,0 +1,24 @@
+# 0 setup --------------
+
+rm(list = ls()) #full reset
+
+library(dplyr)    #activating the package for running code for it.
+library(ggplot2)
+library(stringr)
+library(raster)
+library(sf)
+library(terra)
+library(usethis)
+
+
+wd <- ("C:/Users/repap5991/Downloads/Data/R/GraduationProject") #setting the working dirctory
+setwd (wd) 
+
+
+# 1.0 loading data ---------
+
+
+
+
+
+
---
+++ Readme.md
@@ -0,0 +1,7 @@
+---
+title: "Readme.md"
+output: html_document
+date: "2024-03-18"
+---
+
+## Readme 
